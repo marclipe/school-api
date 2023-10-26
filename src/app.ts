ab26a07d5491c8965f2a46e9a39569ca9ea69992
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-10-26 16:35:40 -0300

    Crete route users and validation of user

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
 import fastify from 'fastify';
-import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
+import { prisma } from './lib/prisma';
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
+app.post('/users', async (request, reply) => {
+    const registerBodySchema = z.object({
+        name: z.string(), 
+        email: z.string().email(), 
+        password: z.string().min(6),
+    });
+    //Validation
+    const { name, email, password } = registerBodySchema.parse(request.body);
 
-prisma.user.create({
-    data: {
-        name: 'MarcLipe', 
-        email: 'marclipe@email.com',
-    }
+    //creating user
+    await prisma.user.create({
+        data: {
+            name, 
+            email,
+            password_hash: password
+        }
+    });
+
+    return reply.status(201).send();
 });
