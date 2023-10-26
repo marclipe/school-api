f72bdd8084229aec1a1844e451c784072d0a3408
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-10-26 17:59:34 -0300

    Controller of Register and create Router User

---
+++ src/ http/controllers/register.controller.ts
@@ -0,0 +1,24 @@
+import { prisma } from '@/lib/prisma';
+import { z } from 'zod';
+import { FastifyRequest, FastifyReply } from 'fastify';
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+    const registerBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6),
+    });
+    //Validation
+    const { name, email, password } = registerBodySchema.parse(request.body);
+
+    //creating user
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password_hash: password,
+        },
+    });
+
+    return reply.status(201).send();
+}
