2aa575621c8f91883fcdaffeee63404b45291525
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-11-08 14:30:46 -0300

    Addding service of Register

--- src/ http/controllers/register.controller.ts
+++ src/ http/controllers/register.controller.ts
@@ -1,7 +1,6 @@
-import { prisma } from '@/lib/prisma';
 import { z } from 'zod';
 import { FastifyRequest, FastifyReply } from 'fastify';
-import { hash } from 'bcryptjs';
+import { registerService } from '@/services/register';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerBodySchema = z.object({
@@ -12,25 +11,15 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     //Validation
     const { name, email, password } = registerBodySchema.parse(request.body);
 
-    const password_hash = await hash(password, 6); //rounds = 6
-
-    const userWithSameEmail = await prisma.user.findUnique({
-        where: {
+    try {
+        await registerService({
+            name, 
             email,
-        },
-    }); 
-
-    if (userWithSameEmail) {
-        return reply.status(409).send();
+            password
+        });
+    } catch (error) {
+        return reply.status(409).send(error);
     }
-
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash
-        },
-    });
-
+    
     return reply.status(201).send();
 }
