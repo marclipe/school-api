75aa7eaec1867c0ca47a017292464ed75f22efd9
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-11-21 20:56:11 -0300

    Handle with errors of use-case

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,5 +1,6 @@
 import { UsersRepository } from '@/repositories/users-repository';
 import { hash } from 'bcryptjs';
+import { UserAlreadyExistsError } from './errors/user-already-exists-error';
 
 interface RegisterServiceRequest {
   name: string;
@@ -19,7 +20,7 @@ export class RegisterService {
         const userWithSameEmail = await this.userRepository.findByEmail(email);
 
         if (userWithSameEmail) {
-            throw new Error('E-mail already exists!');
+            throw new UserAlreadyExistsError();
         }
 
         // const prismaUserRepository = new PrismaUserRepository();
